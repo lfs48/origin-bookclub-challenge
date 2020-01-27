fa5faf182eb088b8225a83ba1e8476ce54c151b1
lucas schraier <lucasfschraier@gmail.com>
2020-01-27 14:41:00 -0500

    Initial commit

---
+++ frontend/bookclub.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+
+const cb = () => {
+
+    const store = configureStore({});
+
+    const root = document.getElementById("root");
+
+    ReactDOM.render(<Root store={store} />, root);
+};
+
+document.addEventListener("DOMContentLoaded", cb);
