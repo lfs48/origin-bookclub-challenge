4a9874a984d91bf69483669c8c3c2938aa4a8420
lucas schraier <lucasfschraier@gmail.com>
2020-01-27 18:17:17 -0500

    Initial commit

---
+++ frontend/components/root.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
+const Root = ({store}) => (
+    <span>"Hello World"</span>
+);
+
+export default Root;
