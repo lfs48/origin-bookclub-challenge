b6fa236deeca0511b9e65a490cf0b8a0e00c7a85
lucas schraier <lucasfschraier@gmail.com>
2020-01-27 21:44:59 -0500

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
 
+    validates :username, :password_digest, :session_token, presence: true
+    validates :username, :session_token, uniqueness: true
+    validates :password, allow_nil: true, length: {minimum: 8}
+
 end
