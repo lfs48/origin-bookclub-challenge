99a6375ec2345f4b021a018c8a9fe69f173867c7
lucas schraier <lucasfschraier@gmail.com>
2020-01-28 14:52:48 -0500

    Handle RECEIVE_USER and RECEIVE_ALL_USERS actions

--- frontend/reducers/entities/users_reducer.js
+++ frontend/reducers/entities/users_reducer.js
@@ -1,11 +1,25 @@
 import { merge } from 'lodash';
+import {RECEIVE_USER, RECEIVE_ALL_USERS} from '../../actions/entities/types';
 
 const usersReducer = (state = {}, action) => {
+    // Copy of state is created and state is frozen to ensure this function does not create side effects
     const newState = merge({}, state);
     Object.freeze(state);
 
     switch (action.type) {
         default: return state;
+
+        // Sets key in new state corresponding to user's id to be user info provided by action
+        // Returns the new state
+        case(RECEIVE_USER): {
+            newState[action.user.id] = action.user
+            return newState;
+        }
+
+        // Returns the users collection provided by action to replace previous state
+        case(RECEIVE_ALL_USERS): {
+            return action.users;
+        }
     }
 };
 
