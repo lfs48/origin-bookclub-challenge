dc89f1633908512fee9f0dc7bfdb5ae35d483f9a
lucas schraier <lucasfschraier@gmail.com>
2020-01-28 14:08:43 -0500

    Add create and destroy methods

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,3 +1,24 @@
 class Api::SessionsController < ApplicationController
 
+    # Validates username and password from params. 
+    # If validated, calls log_user_in application controller function and renders api/users/show view.
+    # If validation fails, renders a descriptive error.
+    def create
+        email = params[:user][:username]
+        pw = params[:user][:password]
+        @user = User.find_by_credentials(email, pw)
+        if @user
+            log_user_in!(@user)
+            render "api/users/show"
+        else
+            render json: "Your email or password were incorrect.", status: 422
+        end
+    end
+
+    # Logs user out by calling log_user_out! function from application controller.
+    def destroy
+        log_user_out!
+    end
+
+
 end
