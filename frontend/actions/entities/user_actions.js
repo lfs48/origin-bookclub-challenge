00294bd076ec56a62710281bd21f4f319a6c8f25
lucas schraier <lucasfschraier@gmail.com>
2020-01-28 14:43:10 -0500

    Remove signup errors action, pending errors implementation

--- frontend/actions/entities/user_actions.js
+++ frontend/actions/entities/user_actions.js
@@ -1,5 +1,5 @@
 import * as UsersAPIUtil from '../../util/api/user_api_util';
-import {RECEIVE_USER, RECEIVE_ALL_USERS, RECEIVE_SIGNUP_ERRORS } from './types';
+import {RECEIVE_USER, RECEIVE_ALL_USERS} from './types';
 
 
 // Standard actions
@@ -15,37 +15,3 @@ const receiveAllUsers = (users) => ({
     type: RECEIVE_ALL_USERS,
     users: users
 });
-
-// Action to add signup errors to state.
-const receiveSignupErrors = (errors) => ({
-    type: RECEIVE_SIGNUP_ERRORS,
-    errors: errors
-});
-
-// Thunk actions
-
-// Makes an http request for a user by id.
-// Then creates and dispatches an action to add that user to state.
-export const fetchUser = (id) => (dispatch) => {
-    return UsersAPIUtil.fetchUser(id).then(
-        (user) => dispatch( receiveUser(user) )
-    );
-};
-
-// Makes an http request for collection of all users.
-// Then creates and dispatches an action to add users to state.
-export const fetchUsers = () => (dispatch) => {
-    return UsersAPIUtil.fetchUsers().then(
-        (users) => dispatch(receiveAllUsers(users))
-    );
-};
-
-// Makes an http request to create a new user from form data.
-// Then creates an action to add the newly created user to state and dispatches the action.
-// If user creation fails, instead creates and dispatches an action to add signup errors to state.
-export const createUser = (formUser) => (dispatch) => {
-    return UsersAPIUtil.createUser(formUser).then(
-        (user) => dispatch(receiveUser(user)),
-        (errors) => dispatch(receiveSignupErrors(errors.responseJSON))
-    );
-};
