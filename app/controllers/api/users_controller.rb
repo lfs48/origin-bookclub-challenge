9b45e0d2cc1a3e8819caf2dba392fbc0ebae5461
lucas schraier <lucasfschraier@gmail.com>
2020-01-27 22:04:58 -0500

    Define index, create, show methods and user_params helper method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,28 @@
 class Api::UsersController < ApplicationController
 
+    def index
+        @users = User.all
+        render "api/users/index"
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render "api/users/show"
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render "api/users/show" if @user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
