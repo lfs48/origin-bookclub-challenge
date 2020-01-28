8fdceec40f2f284c5e57bb19e023d24f12c4092c
lucas schraier <lucasfschraier@gmail.com>
2020-01-28 12:38:19 -0500

    Include users slice of state

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import usersReducer from './entities/users_reducer';
 
 const rootReducer = combineReducers({
-
+    users: usersReducer
 });
 
 export default rootReducer;
