7a6d3ee9f2538fdceb95afc4519e980f096687d8
lucas schraier <lucasfschraier@gmail.com>
2020-01-27 22:09:07 -0500

    Add comments to document method functionality

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,12 @@
 class Api::UsersController < ApplicationController
 
+    # Retrieves all users from db, then renders api/users/index view
     def index
         @users = User.all
         render "api/users/index"
     end
 
+    # Creates a user object from filtered params, then renders api/users/show view if successfully persists to db, or renders errors otherwise
     def create
         @user = User.new(user_params)
         if @user.save
@@ -14,6 +16,7 @@ class Api::UsersController < ApplicationController
         end
     end
 
+    # Looks up user by id from params, then renders api/users/show view if a user with that id is found
     def show
         @user = User.find_by(id: params[:id])
         render "api/users/show" if @user
@@ -21,6 +24,7 @@ class Api::UsersController < ApplicationController
 
     private
 
+    # Filters params so that only params with a key of user is allowed, and only reads the username and password subkeys
     def user_params
         params.require(:user).permit(:username, :password)
     end
