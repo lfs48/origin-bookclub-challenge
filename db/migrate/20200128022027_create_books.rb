8f5058e80f3a132444d8d436b5ce7468fbd2b42a
lucas schraier <lucasfschraier@gmail.com>
2020-01-27 21:25:21 -0500

    Create books table with title, author, genre fields

---
+++ db/migrate/20200128022027_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title, null: false
+      t.string :author, null: false
+      t.string :genre, null: false
+      
+      t.index [:title, :author], unique: true
+      t.index :genre
+      
+      t.timestamps
+    end
+  end
+end
