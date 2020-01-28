0cb235d7607145b21dba0b2ab79755a5625be972
lucas schraier <lucasfschraier@gmail.com>
2020-01-28 18:39:07 -0500

    Wrap all components in provider for access to redux store

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import App from './App';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const Root = ({store}) => (
-    <App></App>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App></App>
+        </BrowserRouter>
+    </Provider>
 );
 
 export default Root;
