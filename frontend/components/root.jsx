c5d30e33ca2e7bdf347b8bdca0f1a54abd9786c3
lucas schraier <lucasfschraier@gmail.com>
2020-01-28 18:23:05 -0500

    Render App component

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import App from './App';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
 const Root = ({store}) => (
-    <span>"Hello World"</span>
+    <App></App>
 );
 
 export default Root;
