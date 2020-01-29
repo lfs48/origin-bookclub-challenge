24d23500f6f120f400ee4c9358f683815e54c4af
lucas schraier <lucasfschraier@gmail.com>
2020-01-29 17:08:29 -0500

    Implement controlled inputs and handle register button

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {createUser} from '../../actions/entities/user_actions'
+import {merge} from 'lodash';
 
 const Splash = () => {
+
+    const [state, setState] = useState({
+        username: "",
+        password: ""
+    });
+
+    const dispatch = useDispatch();
+
+    // Controls input fields by updating react state.
+    const updateInput = (e, field) => {
+        e.preventDefault();
+        const newState = merge({}, state);
+        newState[field] = e.target.value;
+        setState(newState);
+    }
+
+    // Handles register button by dispatching a createUser action, using info from react state to build user object.
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const user = {
+            username: state.username,
+            password: state.password
+        };
+        dispatch(createUser(user));
+    }
+
     return(
         <section id="splash-container">
-            <button>Log In</button>
-            <button>Register</button>
+            <header>New User? Register An Account!</header>
+
+            <input 
+                type="text" 
+                id="splash-username-input" 
+                placeholder="Username"
+                value={state.username}
+                onChange={e => updateInput(e, "username")}
+            ></input>
+
+            <input 
+                type="text" 
+                id="splash-password-input" 
+                placeholder="Password"
+                value={state.password}
+                onChange={e => updateInput(e, "password")}
+            ></input>
+
+            <button onClick={e => handleRegister(e)}>Register</button>
         </section>
     )
 }
