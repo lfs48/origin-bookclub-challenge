6e3d2581742c9f982136e00b4110c7682f774af5
lucas schraier <lucasfschraier@gmail.com>
2020-01-29 13:19:18 -0500

    Add sessions action types

--- frontend/actions/types.js
+++ frontend/actions/types.js
@@ -2,3 +2,8 @@
 
 export const RECEIVE_USER = "RECEIVE_USER";
 export const RECEIVE_ALL_USERS = "RECEIVE_ALL_USERS";
+
+// Sessions
+
+export const LOGIN_USER = "LOGIN_USER";
+export const LOGOUT_USER = "LOGOUT_USER";
