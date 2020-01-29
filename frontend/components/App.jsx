444810c91f9e226fe0a43f93a982d5660cf84a80
lucas schraier <lucasfschraier@gmail.com>
2020-01-29 12:21:28 -0500

    Render navbar

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import Navbar from './navbar/navbar';
 import Splash from './splash/splash';
 
 const App = () => (
     <main id="app-container">
+        <Navbar/>
         <Route path="/" component={Splash} />
     </main>
 );
