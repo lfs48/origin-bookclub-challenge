56a1e7082ab8820b06dfeca7daf6d6d4e5a600fa
lucas schraier <lucasfschraier@gmail.com>
2020-01-29 12:42:23 -0500

    Implement controlled inputs with react state

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {merge} from 'lodash';
 
 const Navbar = () => {
+    const [state, setState] = useState({
+        username: "",
+        password: ""
+    });
+
+    const updateInput = (e, field) => {
+        e.preventDefault();
+        const newState = merge({}, state);
+        newState[field] = e.target.value;
+        setState(newState);
+    }
+
     return(
         <nav id="nav-container">
             <label htmlFor="nav-username-input"></label>
-            <input type="text" id="nav-username-input" placeholder="Username"></input>
+            <input 
+                type="text" 
+                id="nav-username-input" 
+                placeholder="Username"
+                value={state.username}
+                onChange={e => updateInput(e, "username")}
+            ></input>
 
             <label htmlFor="nav-password-input"></label>
-            <input type="password" id="nav-password-input" placeholder="Password"></input>
+            <input 
+                type="password" 
+                id="nav-password-input" 
+                placeholder="Password"
+                value={state.password}
+                onChange={e => updateInput(e, "password")}
+            ></input>
+
             <button id="nav-login-button">Log In</button>
             <button id="nav-register-button">New User? Register an Account!</button>
         </nav>
