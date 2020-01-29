96340da79facdef82a4454b1100b4fb53065b1ce
lucas schraier <lucasfschraier@gmail.com>
2020-01-29 12:04:37 -0500

    Initial commit

---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Splash = () => {
+    return(
+        <section id="splash-container">
+            <button>Log In</button>
+            <button>Register</button>
+        </section>
+    )
+}
+
+export default Splash;
