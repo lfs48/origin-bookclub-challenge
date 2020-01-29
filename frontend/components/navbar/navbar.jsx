2639521d59336ea4536f990bd55bfbc28cbd2bbe
lucas schraier <lucasfschraier@gmail.com>
2020-01-29 18:58:53 -0500

    Add descriptive comments

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,6 +4,8 @@ import {login, logout} from '../../actions/sessions/sessions_actions';
 import {merge} from 'lodash';
 
 const Navbar = () => {
+
+    // Set react state.
     const [state, setState] = useState({
         username: "",
         password: ""
@@ -11,6 +13,7 @@ const Navbar = () => {
 
     const dispatch = useDispatch();
 
+    // Retrieve data from redux state.
     const {loggedIn, currentUser} = useSelector(
         state => ({
             loggedIn: state.sessions.id != null,
@@ -36,6 +39,7 @@ const Navbar = () => {
         dispatch(login(user));
     }
 
+    // Handles logout button by dispatching a logout action.
     const handleLogout = (e) => {
         e.preventDefault();
         dispatch(logout());
@@ -43,6 +47,9 @@ const Navbar = () => {
 
     let content = <></>
     
+    // Conditionally render different content based on whether user is logged in.
+    // When logged in render welcome message and logout button.
+    // When not logged in, render login input fields.
     if (loggedIn) {
         content = 
         <> 
