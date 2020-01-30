16e406e66d863f79165190181df26447ab553058
lucas schraier <lucasfschraier@gmail.com>
2020-01-29 22:03:47 -0500

    Add descriptive comments

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,6 +3,9 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
+// Conditionally set middleware used based on whether environment is development or production.
+// Thunk is used to create and dispatch actions in all environments.
+// Logger is used for debugging in development only.
 let middleware;
 if (process.env.NODE_ENV === 'development') {
     middleware = applyMiddleware(thunk, logger);
@@ -10,6 +13,7 @@ if (process.env.NODE_ENV === 'development') {
      middleware = applyMiddleware(thunk);
 }
 
+// Configure the refux store, passing in the root reducer, a preloaded state, and middleware determined above.
 const configureStore = (preloadedState = {}) => createStore(
     rootReducer,
     preloadedState,
