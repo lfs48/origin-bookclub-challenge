44f4c045d6f251cd3215a8fb948bb21d32d59ac9
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 11:09:35 -0500

    Change protectedroute redirect to /home

--- frontend/components/routes/routes.jsx
+++ frontend/components/routes/routes.jsx
@@ -7,7 +7,7 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => (
         render={
             (props) => {
                 if (loggedIn) {
-                    return (<Redirect to="/dashboard" />);
+                    return (<Redirect to="/home" />);
                 } else {
                     return (<Component {...props} />);
                 }
