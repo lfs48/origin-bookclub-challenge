f6213085c6d492fb0aee130c11d98c7c3e88eb68
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 14:45:04 -0500

    Add favorite through user rather than through book

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,11 +1,11 @@
 class Api::FavoritesController < ApplicationController
 
     def create
-        @book = Book.find_by(id: params[:favorite][:book_id])
-        if @book.favorites.create(favorites_params)
-            render "api/books/show"
+        @user = User.find_by(id: params[:favorite][:user_id])
+        if @user.favorites.create(favorites_params)
+            render "api/users/show"
         else
-            render json: @book.errors.full_messages, status: 422
+            render json: @user.errors.full_messages, status: 422
         end
     end
 
@@ -20,7 +20,7 @@ class Api::FavoritesController < ApplicationController
     private
 
     def favorites_params
-        params.require(:like).permit(:post_id, :user_id)
+        params.require(:favorite).permit(:user_id, :book_id)
     end
 
 end
