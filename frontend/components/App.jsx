73ad58b4fc20bd47d05e6d46ec2197dc1ad639f5
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 16:16:28 -0500

    Add book route

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,6 +6,7 @@ import Splash from './splash/splash';
 import Home from './home/home';
 import Books from './books/booklist';
 import Bookform from './books/bookform';
+import Book from './books/book';
 import { AuthRoute, ProtectedRoute } from './routes/routes';
 import {fetchBooks} from '../actions/entities/book_actions';
 
@@ -23,6 +24,7 @@ const App = () => {
         <ProtectedRoute path="/create_book" component={Bookform} />
         <ProtectedRoute path="/favorites" component={Books} />
         <ProtectedRoute path="/books" component={Books} />
+        <ProtectedRoute path="/book/:id" component={Book} />
         <ProtectedRoute path="/home" component={Home} />
         <AuthRoute path="/" component={Splash} />
     </main>
