744d652125c539ee6248cbf47f812e5c9ef50407
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 16:16:59 -0500

    Render book info based on id in url

--- frontend/components/books/book.jsx
+++ frontend/components/books/book.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 
-const Book = () => {
-    return(<></>);
+const Book = ({match}) => {
+
+    const {book} = useSelector(
+        state => ({
+            book: state.entities.books[match.params.id] || {title:"",author:"",genre:""}
+        })
+    );
+
+    return(
+        <section id="book-container">
+            <header>Title: {book.title}</header>
+            <span>Author: {book.author}</span>
+            <span>Genre: {book.genre}</span>
+        </section>
+    );
 }
 
-export const Book;
+export default Book;
