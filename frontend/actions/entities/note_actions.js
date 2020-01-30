68f7466019a57c6ff6ce1c28789262fbc7275cb2
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 16:28:02 -0500

    Replace erronous instances of 'book' with 'note'

--- frontend/actions/entities/note_actions.js
+++ frontend/actions/entities/note_actions.js
@@ -1,4 +1,4 @@
-import * as notesAPIUtil from '../../util/api/books_api_util';
+import * as notesAPIUtil from '../../util/api/notes_api_util';
 import {RECEIVE_NOTE, RECEIVE_USER_NOTES, REMOVE_NOTE} from '../types';
 
 
@@ -43,19 +43,19 @@ export const fetchNotes = (userId) => (dispatch) => {
 // Then creates an action to add the newly created note to state and dispatches the action.
 // If note creation fails, instead creates and dispatches an action to add errors to state.
 export const createNote = (formNote) => (dispatch) => {
-    return booksAPIUtil.createNote(formNote).then(
+    return notesAPIUtil.createNote(formNote).then(
         (note) => dispatch(receiveNote(note))
     );
 };
 
 export const updateNote = (formNote) => (dispatch) => {
-    return booksAPIUtil.updateNote(formNote).then(
+    return notesAPIUtil.updateNote(formNote).then(
         (note) => dispatch(receiveNote(note))
     );
 };
 
 export const deleteNote = (id) => (dispatch) => {
-    return booksAPIUtil.deleteNote(id).then(
+    return notesAPIUtil.deleteNote(id).then(
         () => dispatch(removeNote(id))
     );
 };
