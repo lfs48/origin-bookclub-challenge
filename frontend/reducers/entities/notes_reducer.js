1fb10d45e748eb7ab950fa35fb9737d4447fcd2b
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 16:29:45 -0500

    Initial commit

---
+++ frontend/reducers/entities/notes_reducer.js
@@ -0,0 +1,15 @@
+import { merge } from 'lodash';
+import {RECEIVE_NOTE, RECEIVE_USER_NOTES, REMOVE_NOTE} from '../../actions/types';
+
+const notesReducer = (state = {}, action) => {
+    // Copy of state is created and state is frozen to ensure this function does not create side effects
+    const newState = merge({}, state);
+    Object.freeze(state);
+
+    switch (action.type) {
+        default: return state;
+
+    }
+};
+
+export default notesReducer;
