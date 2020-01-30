54b29a7b9e6b84560445beb32a1b25ff33b7b787
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 12:31:29 -0500

    Handle REMOVE_BOOK case

--- frontend/reducers/entities/books_reducer.js
+++ frontend/reducers/entities/books_reducer.js
@@ -1,5 +1,5 @@
 import { merge } from 'lodash';
-import {RECEIVE_BOOK, RECEIVE_ALL_BOOKS} from '../../actions/types';
+import {RECEIVE_BOOK, RECEIVE_ALL_BOOKS, REMOVE_BOOK} from '../../actions/types';
 
 const booksReducer = (state = {}, action) => {
     // Copy of state is created and state is frozen to ensure this function does not create side effects
@@ -12,13 +12,18 @@ const booksReducer = (state = {}, action) => {
         // Sets key in new state corresponding to book's id to be book info provided by action
         // Returns the new state
         case(RECEIVE_BOOK): {
-            newState[action.book.id] = action.book
+            newState[action.book.id] = action.book;
             return newState;
         }
 
         // Returns the books collection provided by action to replace previous state
         case(RECEIVE_ALL_BOOKS): {
             return action.books;
+        }
+
+        case(REMOVE_BOOK): {
+            delete newState[action.id];
+            return newState;
         }
     }
 };
