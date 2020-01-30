4e90ea4afcf06acec1d72d67b4ba70a0f3787c1d
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 11:09:06 -0500

    Link to books list

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
+
     return(
         <section id="home-container">
             <header>Origin Bookclub</header>
-            <button>Browse Books</button>
+            <Link to="/books">Browse Books</Link>
             <button>Favorites</button>
         </section>
     );
