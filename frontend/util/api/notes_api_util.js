d074cd1d1be4cc602b29d866d6a4e6b6739f5803
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 16:01:47 -0500

    Initial commit

---
+++ frontend/util/api/notes_api_util.js
@@ -0,0 +1,52 @@
+// Each of these methods sends an http request using jquery's ajax method to the rails api.
+
+// GET request for a note by id.
+// id should be an integer.
+export const fetchNote = (id) => {
+    return $.ajax({
+        method: 'GET',
+        url: `api/notes/${id}`
+    });
+};
+
+// GET request for collection of all notes.
+export const fetchNotes = (userId) => {
+    return $.ajax({
+        method: 'GET',
+        url: 'api/notes',
+        data: {
+            note: {
+                user_id: userId
+            }
+        }
+    });
+};
+
+// POST request to create a new note.
+// note should be json object containing title, author, and genre keys.
+export const createNote = (note) => {
+    return $.ajax({
+        method: 'POST',
+        url: 'api/notes',
+        data: { note: note }
+    });
+};
+
+// POST request to update a new note.
+// note should be json object containing title, author, and genre keys.
+export const updateNote = (note) => {
+    return $.ajax({
+        method: 'PATCH',
+        url: `api/notes/${note.id}`,
+        data: { note: note }
+    });
+};
+
+
+// POST request to delete a note by id.
+export const deleteNote = (id) => {
+    return $.ajax({
+        method: 'DELETE',
+        url: `api/notes/${id}`
+    });
+};
