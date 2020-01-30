e7beb6f87e144eeb8b6a87995479f5f5c46f6493
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 16:30:10 -0500

    Include notes slice of state

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import booksReducer from './books_reducer';
+import notesReducer from './notes_reducer';
 
 // Combines all lower-level entities reducers, to be passed to the root reducer under the 'entities' key.
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    books: booksReducer
+    books: booksReducer,
+    notes: notesReducer
 });
 
 export default entitiesReducer;
