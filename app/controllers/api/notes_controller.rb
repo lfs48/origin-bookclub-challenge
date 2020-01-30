944f53ac2f46291277de119ad4e4d8e6fee4dca9
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 15:47:16 -0500

    Add index, create, show, update, destroy methods and note_params, get_note, ensure_author helper methods

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,23 +1,50 @@
 class Api::NotesController < ApplicationController
 
-    def index
+    before_action :get_note, only: [:show, :update, :destroy]
+    before_action :ensure_author, only: [:update, :destroy]
 
+    def index
+        @notes = Note.all
+        render "api/notes/index"
     end
 
     def create
-
+        @note = Note.new(note_params)
+        if @note.save
+            render "api/notes/show"
+        else
+            render json: @note.errors.full_messages, status: 422
+        end
     end
 
     def show
-
+        render "api/notes/show"
     end
 
     def update
-
+        if @note.update(note_params)
+            render "api/notes/show"
+        else
+            render @note.errors.full_messages, status: 422
+        end
     end
 
     def destroy
+        @note.destroy
+    end
 
+    private
+
+    def note_params
+        params.require(:note).permit(:user_id, :book_id, :body)
+    end
+
+    def get_note
+        @note = Note.find(params[:id])
+    end
+
+    def ensure_author
+        render json: "You don't have permission to do that", status: 422 unless @note.user_id == current_user.id
     end
 
 end
