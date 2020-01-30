442a50c8ce8267ea6800af5aab8841bf40232100
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 14:44:39 -0500

    Add favorites routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :create, :show]
     resource :session, only: [:create, :destroy]
     resources :books, only: [:index, :create, :show, :update, :destroy]
+    resources :favorites, only: [:create, :destroy]
   end
 
 end
