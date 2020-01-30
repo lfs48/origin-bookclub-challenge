a67ec517eede25d8985c53aefc1194910dbd8627
lucas schraier <lucasfschraier@gmail.com>
2020-01-29 22:33:13 -0500

    Make splash page an authroute, add protetedroute to home page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import Navbar from './navbar/navbar';
 import Splash from './splash/splash';
+import Home from './home/home';
+import { AuthRoute, ProtectedRoute } from './routes/routes';
 
 const App = () => (
     <main id="app-container">
         <Navbar/>
-        <Route path="/" component={Splash} />
+        <ProtectedRoute path="/" component={Home} />
+        <AuthRoute path="/" component={Splash} />
     </main>
 );
 
