1f244bd3d7cc5ea997c5d44241c9f24d7d7955e5
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 14:04:24 -0500

    Add associations

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,4 +3,7 @@ class Favorite < ApplicationRecord
     validates :user_id, :book_id, presence: true
     validates :user_id, uniqueness: {scope: :book_id}
 
+    belongs_to :user, inverse_of: :favorites
+
+    belongs_to :book, inverse_of: :favorites
 end
