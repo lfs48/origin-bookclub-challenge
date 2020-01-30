804446e0f8d502a349d521d83c79c4d4778fc64b
lucas schraier <lucasfschraier@gmail.com>
2020-01-29 23:57:51 -0500

    Dispatch request to load all books into state when app mounts

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
 import {Route} from 'react-router-dom';
 import Navbar from './navbar/navbar';
 import Splash from './splash/splash';
 import Home from './home/home';
 import { AuthRoute, ProtectedRoute } from './routes/routes';
+import {fetchBooks} from '../actions/entities/book_actions';
 
-const App = () => (
+const App = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {dispatch(fetchBooks(), []);
+    });
+
+    return(
     <main id="app-container">
         <Navbar/>
         <ProtectedRoute path="/" component={Home} />
         <AuthRoute path="/" component={Splash} />
     </main>
-);
+    );
+};
 
 export default App;
