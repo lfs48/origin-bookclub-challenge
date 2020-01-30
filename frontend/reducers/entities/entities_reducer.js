d8d4aefb07dc193b7076e4fe8edef79f26305af0
lucas schraier <lucasfschraier@gmail.com>
2020-01-29 19:03:34 -0500

    Add descriptive comments

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import usersReducer from './entities/users_reducer';
 import sessionsReducer from './sessions/sessions_reducer'
+
+// Combines all lower-level entities reducers, to be passed to the root reducer under the 'entities' key.
 const entitiesReducer = combineReducers({
     users: usersReducer
 });
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import entitiesReducer from './entities/users_reducer';
 import sessionsReducer from './sessions/sessions_reducer';
 
+// Combines all other high-level reducers to generate redux state.
 const rootReducer = combineReducers({
     entities: entitiesReducer,
     sessions: sessionsReducer
