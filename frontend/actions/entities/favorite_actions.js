2f0a176602234eced9eaaf18e988b33f7f708b68
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 15:26:42 -0500

    Add removeFavorite action

--- frontend/actions/entities/favorite_actions.js
+++ frontend/actions/entities/favorite_actions.js
@@ -3,9 +3,16 @@ import {receiveUser} from './user_actions';
 
 // Makes an http request to create a new like from data.
 // Then creates an action to add the user adding the favorite to state and dispatches the action.
-// Data should contain a userId and bookId key.
 export const createFavorite = (userId, bookId) => (dispatch) => {
     return FavoritesAPIUtil.favorite(userId, bookId).then(
+        (user) => dispatch(receiveUser(user))
+    );
+};
+
+// Makes an http request to remove a favorite.
+// Then creates an action to add the user removing the favorite to state and dispatches the action.
+export const removeFavorite = (userId, bookId) => (dispatch) => {
+    return FavoritesAPIUtil.unfavorite(userId, bookId).then(
         (user) => dispatch(receiveUser(user))
     );
 };
