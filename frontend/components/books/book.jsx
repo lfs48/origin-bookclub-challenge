5ec246dfde866470a53a219426796c8d07a79a1f
lucas schraier <lucasfschraier@gmail.com>
2020-01-30 16:27:45 -0500

    Implement controlled inputs and handle note submission

--- frontend/components/books/book.jsx
+++ frontend/components/books/book.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, {useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {createNote} from '../../actions/entities/note_actions';
+import {merge} from 'lodash';
 
 const Book = ({match}) => {
 
-    const {book} = useSelector(
+    const dispatch = useDispatch();
+
+    const [state, setState] = useState({
+        noteBody: ""
+    });
+
+    const {currentUser, book} = useSelector(
         state => ({
-            book: state.entities.books[match.params.id] || {title:"",author:"",genre:""}
+            currentUser: state.entities.users[state.sessions.id],
+            book: state.entities.books[match.params.id] || {id:"",title:"",author:"",genre:""}
         })
     );
+
+    const handleInput = (e, field) => {
+        e.preventDefault();
+        const newState = merge({}, state);
+        newState[field] = e.target.value;
+        setState(newState);
+    }
+
+    const handleAddNote = (e) => {
+        e.preventDefault();
+        const note = {
+            user_id: currentUser.id,
+            book_id: book.id,
+            body: state.noteBody
+        }
+        dispatch(createNote(note));
+    }
 
     return(
         <section id="book-container">
             <header>Title: {book.title}</header>
             <span>Author: {book.author}</span>
             <span>Genre: {book.genre}</span>
+            <input
+                type="text"
+                value={state.noteBody}
+                placeholder="Type your note here"
+                onChange={e => handleInput(e, "noteBody")}
+            ></input>
+            <button onClick={e => handleAddNote(e, "noteBody")}>Add Note</button>
         </section>
     );
 }
