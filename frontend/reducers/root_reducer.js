9eea6e3b53f35cc0ba82b0df811fbbce83c7d36a
lucas schraier <lucasfschraier@gmail.com>
2020-01-31 12:57:06 -0500

    Include errors slice of state

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import entitiesReducer from './entities/entities_reducer';
 import sessionsReducer from './sessions/sessions_reducer';
+import errorsReducer from './errors/errors_reducer';
 
 // Combines all other high-level reducers to generate redux state.
 const rootReducer = combineReducers({
     entities: entitiesReducer,
-    sessions: sessionsReducer
+    sessions: sessionsReducer,
+    errors: errorsReducer
 });
 
 export default rootReducer;
